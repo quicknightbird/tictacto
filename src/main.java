889cafd904472cc0c9414f0a53127ee033a188c8
quicknightbird <eliasriv73@gmail.com>
2022-05-20 01:39:25 +0300

    new files

--- src/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("FOR GIT");
-        System.out.println("FOR GIT");
-        System.out.println("FOR GIT");
-    }
-}
---
+++ src/tictactoe.java
@@ -0,0 +1,5 @@
+public class tictactoe {
+    public static void main(String[] args) {
+        System.out.println("FOR GIT");
+    }
+}
